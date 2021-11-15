3343d85902fe00c0625d2762964701eca3c26822
Naji Dia <najimdia@gmail.com>
2021-11-14 22:08:31 -0800

    updates

--- script.js
+++ script.js
@@ -9,26 +9,24 @@ cityChoiceAr = [];
 
 cityinputEl.keydown(function (event) {
     if (event.keyCode == 13) {
-      event.preventDefault();
-      const inputValue = cityinputEl.val();
-      cityChoiceAr.push(inputValue);
-      console.log(cityChoiceAr);
-      if(!inputValue) {
-      } else {
-        history.append(`
-        <li><button>${inputValue}</button></li>`)
-      }
-      $("input").val("");
+        event.preventDefault();
+        var inputValue = cityinputEl.val();
+        cityChoiceAr.push(inputValue); 
+        if(!inputValue) {
+        } else {
+            history.append(`
+            <li class="inputValue"><button>${inputValue}</button></li>`)
+        }
+        $("input").val("");
     }
+    
 });
-  
 
-
-const findCityName = (inputValue) => {
+const findCityName = (finalValue) => {
+    console.log(finalValue)
     $("#apiCall").innerHTML = '';
-    localStorage.setItem('searchHistory', JSON.stringify(inputValue));
-    console.log(inputValue);
-    var urlRequest = 'https://api.openweathermap.org/data/2.5/weather?q='+inputValue+'&appid=376b31c0d35b891d69be7dac3c604407'
+    localStorage.setItem('.inputValue', JSON.stringify(finalValue));
+    var urlRequest = 'https://api.openweathermap.org/data/2.5/weather?q='+finalValue+'&appid=376b31c0d35b891d69be7dac3c604407'
     fetch(urlRequest)
     .then(function (response) {
       return response.json();
@@ -41,16 +39,12 @@ const findCityName = (inputValue) => {
       console.log(lon);
       var urlCall = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lon+'&units=imperial&APPID=376b31c0d35b891d69be7dac3c604407'
       fetch(urlCall)
-      .then(function (response) {
-          return response.json();
-        });
-        .then(function (data) {
+      .then(function (response) {return response.json();}).then(function (data) {
             console.log(data)
         });
     });  
 };
 
-
 submitBtn.on("click", onClick = () => {
-    findCityName()
+    findCityName(finalValue)
 });
